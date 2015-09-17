d63a687af6bf3376ef9cda63e35d43b5be909f28
zhengna <zhengna@zhengnadeMac.local>
2015-09-17 03:52:45 -0700

    setup project

---
+++ ChatHere/MessageItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessageItemViewController.h
+//  ChatHere
+//
+//  Created by zhengna on 15/9/17.
+//  Copyright (c) 2015年 zhengna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageItemViewController : UITableViewController
+
+@end
---
+++ ChatHere/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatHere
+//
+//  Created by zhengna on 15/9/17.
+//  Copyright (c) 2015年 zhengna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
